14d1daeae3dd2569f175e0210ad5ff418531bdde
Irman23 <73824877+Irman23@users.noreply.github.com>
2021-03-16 10:48:44 +0100

    day08

---
+++ ex08/src/index.js
@@ -0,0 +1,10 @@
+var total = 0;
+function funcWithArg(digit){
+    return(digit + 9) / 5
+}
+total = funcWithArg(6);
+// Only change code below this line
+
+// Only change code above this line
+console.log(total);
+module.exports = funcWithArg;
---
+++ ex13/src/index.js
@@ -0,0 +1,8 @@
+function randomRangeNumber (minNumber, maxNumber){
+    return Math.floor(Math.random() * (maxNumber - minNumber + 1)) + minNumber;
+}
+
+var randomRangeNumber = randomRangeNumber(5, 15);
+console.log(randomRangeNumber);
+module.exports = randomRangeNumber;
+
